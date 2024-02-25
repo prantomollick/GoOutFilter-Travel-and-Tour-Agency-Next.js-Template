ad26e7217e24b9e8e5488e049b524371134ba11a
Pranto Mollick <prantocse01@gmail.com>
2024-02-25 19:00:11 +0600

    hero section design completed

--- src/app/home/components/hero/hero.tsx
+++ src/app/home/components/hero/hero.tsx
@@ -1,17 +1,38 @@
+"use client";
+import classNames from "classnames";
+import SearchForm from "../search-form/search-form";
 import styles from "./hero.module.scss";
-import React from "react";
+import React, { useState } from "react";
+type Nav = {
+  label: string;
+  isActive: boolean;
+};
 
 function Hero() {
-  const navs = [
-    "Hotel",
-    "Tour",
-    "Activity",
-    "Holiday",
-    "Rentals",
-    "Car",
-    "Cruise",
-    "Flights"
+  const [isNavActive, setIsNavActive] = useState({
+    label: "Hotel",
+    isActive: true
+  });
+
+  let navs: Nav[] = [
+    { label: "Hotel", isActive: false },
+    { label: "Tour", isActive: false },
+    { label: "Activity", isActive: false },
+    { label: "Holiday", isActive: false },
+    { label: "Rentals", isActive: false },
+    { label: "Car", isActive: false },
+    { label: "Cruise", isActive: false },
+    { label: "Flights", isActive: false }
   ];
+
+  const handleClick = (nav: Nav) => {
+    setIsNavActive(nav);
+    navs = navs.map((navItem) =>
+      navItem.label === nav.label
+        ? { ...navItem, isActive: true }
+        : { ...navItem, isActive: false }
+    );
+  };
 
   return (
     <section id="#hero" className={styles["hero-section"]} role="banner">
@@ -25,11 +46,20 @@ function Hero() {
         <div className={styles["searchNav"]}>
           <ul className={styles["searchNav__list"]}>
             {navs.map((nav) => (
-              <li key={nav} className={styles["searchNav__item"]}>
-                {nav}
+              <li
+                key={nav.label}
+                className={classNames(
+                  styles["searchNav__item"],
+                  isNavActive.label === nav.label && "border-b-white"
+                )}
+                onClick={() => handleClick(nav)}
+              >
+                {nav.label}
               </li>
             ))}
           </ul>
+
+          {isNavActive && <SearchForm />}
         </div>
       </div>
     </section>
